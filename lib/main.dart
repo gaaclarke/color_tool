97321104aa40d26141569d753d2564f3a5221516
Aaron Clarke <aaclarke@google.com>
2024-07-03 10:00:13 -0700

    tweak

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,106 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const ColorSelectorApp());
 }
 
+enum ColorSpace { sRGB, displayP3 }
+
+class WideColor {
+  final double a;
+  final double r;
+  final double g;
+  final double b;
+  final ColorSpace colorSpace;
+
+  WideColor.fromARGB(this.a, this.r, this.g, this.b, this.colorSpace);
+
+  WideColor withRed(double red) => WideColor.fromARGB(a, red, g, b, colorSpace);
+  WideColor withGreen(double green) =>
+      WideColor.fromARGB(a, r, green, b, colorSpace);
+  WideColor withBlue(double blue) =>
+      WideColor.fromARGB(a, r, g, blue, colorSpace);
+
+  WideColor toSRGB() {
+    if (colorSpace == ColorSpace.sRGB) {
+      return this;
+    } else {
+      return Matrix(<double>[
+        1.22494, -0.22494, 0, //
+        -0.0420568, 1.04206, 0, //
+        -0.0196327, -0.0788819, 1.09851
+      ]).transform(this, ColorSpace.sRGB);
+    }
+  }
+
+  WideColor toDisplayP3() {
+    if (colorSpace == ColorSpace.displayP3) {
+      return this;
+    } else {
+      return Matrix(<double>[
+        0.8224621, 0.1775379, 0.0000000, //
+        0.0331941, 0.9668059, 0.0000000, //
+        0.0170827, 0.0725972, 0.9103202
+      ]).transform(this, ColorSpace.displayP3);
+    }
+  }
+}
+
+class Matrix {
+  final List<double> values;
+
+  /// Row-major.
+  Matrix(this.values);
+
+  WideColor transform(WideColor color, ColorSpace resultColorSpace) {
+    return WideColor.fromARGB(
+        color.a,
+        values[0] * color.r + values[1] * color.g + values[2] * color.b,
+        values[3] * color.r + values[4] * color.g + values[5] * color.b,
+        values[6] * color.r + values[7] * color.g + values[8] * color.b,
+        resultColorSpace);
+  }
+}
+
+Future<ui.Image> createSolidColorImage(
+    WideColor color, int width, int height) async {
+  WideColor srgbColor = color.toSRGB();
+  final pixels = Float32List.fromList(
+    List.generate(width * height * 4, (index) {
+      final pixelIndex = index ~/ 4;
+      final x = pixelIndex % width;
+      final y = pixelIndex ~/ width;
+      if (x >= 0 && x < width && y >= 0 && y < height) {
+        switch (index % 4) {
+          case 0:
+            return srgbColor.r;
+          case 1:
+            return srgbColor.g;
+          case 2:
+            return srgbColor.b;
+          case 3:
+            return srgbColor.a;
+        }
+      }
+      return 0;
+    }),
+  );
+
+  final completer = Completer<ui.Image>();
+  ui.decodeImageFromPixels(
+      Uint8List.view(pixels.buffer), width, height, ui.PixelFormat.rgbaFloat32,
+      (ui.Image img) {
+    completer.complete(img);
+  });
+  return completer.future;
+}
+
 class ColorSelectorApp extends StatelessWidget {
   const ColorSelectorApp({super.key});
+  final Color foo = const ui.Color.fromARGB(255, 169, 10, 10);
 
   @override
   Widget build(BuildContext context) {
@@ -27,18 +122,37 @@ class _ColorSelectorPage extends StatefulWidget {
 }
 
 class _ColorSelectorPageState extends State<_ColorSelectorPage> {
-  Color _leftColor = Colors.red;
-  Color _rightColor = Colors.blue;
+  WideColor _leftColor = WideColor.fromARGB(1, 1, 0, 0, ColorSpace.sRGB);
+  WideColor _rightColor = WideColor.fromARGB(1, 1, 0, 0, ColorSpace.sRGB);
+  ui.Image? _leftPreview;
+  ui.Image? _rightPreview;
 
-  void _updateColor(Color color, bool isLeft) {
+  @override
+  void initState() {
+    _generateLeftColorImage();
+    _generateRightColorImage();
+  }
+
+  void _updateColor(WideColor color) {
     setState(() {
-      if (isLeft) {
-        _leftColor = color;
-        _rightColor = color;
-      } else {
-        _rightColor = color;
-        _leftColor = color;
-      }
+      _leftColor = color.toSRGB();
+      _rightColor = color.toDisplayP3();
+    });
+    _generateLeftColorImage();
+    _generateRightColorImage();
+  }
+
+  void _generateLeftColorImage() async {
+    final image = await createSolidColorImage(_leftColor, 100, 100);
+    setState(() {
+      _leftPreview = image;
+    });
+  }
+
+  void _generateRightColorImage() async {
+    final image = await createSolidColorImage(_rightColor, 100, 100);
+    setState(() {
+      _rightPreview = image;
     });
   }
 
@@ -56,16 +170,17 @@ class _ColorSelectorPageState extends State<_ColorSelectorPage> {
               child: ColorSelector(
                 label: 'sRGB',
                 color: _leftColor,
-                onColorChanged: (color) => _updateColor(color, true),
+                onColorChanged: (color) => _updateColor(color),
+                preview: _leftPreview,
               ),
             ),
             const SizedBox(width: 16),
             Expanded(
               child: ColorSelector(
-                label: 'DisplayP3',
-                color: _rightColor,
-                onColorChanged: (color) => _updateColor(color, false),
-              ),
+                  label: 'DisplayP3',
+                  color: _rightColor,
+                  onColorChanged: (color) => _updateColor(color),
+                  preview: _rightPreview),
             ),
           ],
         ),
@@ -76,14 +191,16 @@ class _ColorSelectorPageState extends State<_ColorSelectorPage> {
 
 class ColorSelector extends StatelessWidget {
   final String label;
-  final Color color;
-  final ValueChanged<Color> onColorChanged;
+  final WideColor color;
+  final ValueChanged<WideColor> onColorChanged;
+  final ui.Image? preview;
 
   const ColorSelector(
       {super.key,
       required this.label,
       required this.color,
-      required this.onColorChanged});
+      required this.onColorChanged,
+      required this.preview});
 
   @override
   Widget build(BuildContext context) {
@@ -91,34 +208,34 @@ class ColorSelector extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(label),
-        Container(
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-            color: color,
-            border: Border.all(color: Colors.black38, width: 2),
-          ),
-        ),
+        if (preview == null)
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.grey,
+          )
+        else
+          RawImage(image: preview, width: 100.0, height: 100.0),
         const SizedBox(height: 16),
         ColorSlider(
           label: 'Red',
-          value: color.red.toDouble(),
+          value: color.r,
           onChanged: (value) {
-            onColorChanged(color.withRed(value.toInt()));
+            onColorChanged(color.withRed(value));
           },
         ),
         ColorSlider(
           label: 'Green',
-          value: color.green.toDouble(),
+          value: color.g,
           onChanged: (value) {
-            onColorChanged(color.withGreen(value.toInt()));
+            onColorChanged(color.withGreen(value));
           },
         ),
         ColorSlider(
           label: 'Blue',
-          value: color.blue.toDouble(),
+          value: color.b,
           onChanged: (value) {
-            onColorChanged(color.withBlue(value.toInt()));
+            onColorChanged(color.withBlue(value));
           },
         ),
       ],
@@ -140,17 +257,20 @@ class ColorSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Text(label),
-        Expanded(
-          child: Slider(
-            value: value,
-            min: 0,
-            max: 255,
-            onChanged: onChanged,
-          ),
+        Row(
+          children: [
+            Text('$label: '),
+            Text(value.toStringAsFixed(2)),
+          ],
         ),
+        Slider(
+          value: value,
+          min: -0.5,
+          max: 2.0,
+          onChanged: onChanged,
+        )
       ],
     );
   }
